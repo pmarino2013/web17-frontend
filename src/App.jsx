a749332febe83dc6cb2da521bfe2221351819e15
Pablo Marino <pmarino2013@gmail.com>
2026-02-09 20:34:12 -0300

    Agrego las urls de redirección para mercado pago pero no me funciona desde el back

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import{BrowserRouter, Routes, Route} from 'react-router-dom'
-import LoginScreen from './views/LoginScreen'
-import HomeScreen from './views/HomeScreen'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./views/LoginScreen";
+import HomeScreen from "./views/HomeScreen";
+import Error404Screen from "./views/Error404Screen";
 
 export default function App() {
   return (
@@ -9,7 +10,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LoginScreen />} />
         <Route path="/home" element={<HomeScreen />} />
+        <Route path="/success" element={<HomeScreen />} />
+
+        <Route path="/failure" element={<Error404Screen />} />
+
+        <Route path="/*" element={<Error404Screen />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
