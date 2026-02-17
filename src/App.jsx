080adceb501a47bc775bb0ddf90ecf67dc71d778
Pablo Marino <pmarino2013@gmail.com>
2026-02-17 14:50:12 -0300

    Creo el context de usuario con su función para guardar el perfil y agrego ruta protegida

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import HomeScreen from "./views/HomeScreen";
 import Error404Screen from "./views/Error404Screen";
 import ProductSearchScreen from "./views/ProductSearchScreen";
 import TemplateLayout from "./layout/TemplateLayout";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
-        <Route path="/" element={<TemplateLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <TemplateLayout />
+            </ProtectedRoutes>
+          }
+        >
           <Route index element={<HomeScreen />} />
           <Route path="/buscar/:termino" element={<ProductSearchScreen />} />
         </Route>
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,13 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext(null);
 
 const UserProvider = ({ children }) => {
-  const user = {
-    name: "Pablo",
-    email: " ",
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    role: "",
+  });
+
+  const loadUserData = async () => {
+    try {
+      const response = await fetch("http://localhost:9005/api/auth/profile", {
+        credentials: "include", // para enviar cookies
+      });
+      if (response.ok) {
+        const { data } = await response.json();
+        setUser({
+          username: data.username,
+          email: data.email,
+          role: data.role,
+        });
+      } else {
+        setUser(null); // Si no se pudo cargar, se considera no autenticado
+      }
+    } catch (error) {
+      console.error("Error al cargar datos de usuario:", error);
+      setUser(null);
+    }
   };
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  //funcion para cargar datos de usuario desde el backend
+
+  return (
+    <UserContext.Provider value={{ user, loadUserData }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
-export default UserProvider;
+export { UserProvider, UserContext };
